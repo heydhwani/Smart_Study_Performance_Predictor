1a92a8847f24a36b03918c3685f5ba1b9f4b7674
Dhwani <dhwanijain731@gmail.com>
2026-07-27 23:28:09 +0530

    notebook completed

--- src/predict.py
+++ src/predict.py
@@ -12,27 +12,20 @@ classification_scaler = joblib.load("models/classification_scaler.pkl")
 
 
 def predict(data):
-    """
-    Predict exam score and performance category.
-    """
 
     df = preprocess_input(data)
 
-    # Regression
     reg_input = regression_scaler.transform(df)
     predicted_score = regression_model.predict(reg_input)[0]
 
-    # Classification
     cls_input = classification_scaler.transform(df)
     predicted_class = classification_model.predict(cls_input)[0]
 
-    performance = {
-        0: "Low",
-        1: "Medium",
-        2: "High"
-    }
+    print("Predicted class:", predicted_class)
+    print("Predicted score:", predicted_score)
+    print(df)
 
     return {
-        "Predicted Score": round(predicted_score, 2),
-        "Performance": performance[predicted_class]
+        "Predicted Score": round(predicted_score,2),
+        "Performance": predicted_class
     }
